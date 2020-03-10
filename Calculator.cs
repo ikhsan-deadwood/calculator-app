377a98de5a7e94ae5ce7b5444eaee82020edc024
ikhsan <ikhsan.2775@students.amikom.ac.id>
2020-03-10 12:17:15 +0700

    calculator

---
+++ Calculator.cs
@@ -0,0 +1,24 @@
+class Calculator
+{
+	static void Main(string[] args)
+	{
+		int a = 10;
+		int b = 6;
+		
+		Console.Writeline("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a,b));
+		Console.Writeline("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a,b));
+		
+		Console.Writeline("\nTekan Sembarang key untuk keluar");
+		Console.Readkey();
+	}
+	
+	static int Penambahan(int a, int b)
+	{
+		return a + b;
+	}
+	
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
+	}
+}
